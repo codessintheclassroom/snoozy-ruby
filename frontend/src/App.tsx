c567c17aec9005deb20dee0794f70621a15a4bfc
alycen <alsunnid@gmail.com>
2019-05-15 18:47:53 +0100

    Cards working - finished first part of the tutorial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PetCard from './PetCard';
+import { Container, Row, Col, CardColumns } from 'react-bootstrap';
+
+var mockedPets = [
+  { id: "1", name: "Berty", description: "Has a good nose for truffles" },
+  { id: "2", name: "Argo", description: "A superhero (of the dog world)" },
+  { id: "3", name: "Fred", description: "Has opinions about sausages" },
+];
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+      <Col>
+        <CardColumns>
+        {
+          mockedPets.map((pet) => <PetCard key={pet.id} pet={pet} />)
+        }
+        </CardColumns>
+      </Col>
+      </Row>
+    </Container>
   );
 }
 
